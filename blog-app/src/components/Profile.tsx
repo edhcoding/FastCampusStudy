065ddc4da9b8d6683ca5c9f176d18d7349311d50
eundonghyeok <mephisto7191@naver.com>
2024-07-29 16:50:51 +0900

    firebase auth로 로그아웃 기능 구현

--- blog-app/src/components/Profile.tsx
+++ blog-app/src/components/Profile.tsx
@@ -1,16 +1,36 @@
-import { Link } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "firebaseApp";
+import { toast } from "react-toastify";
 
 export function Profile() {
+  const auth = getAuth(app);
+  console.log(auth);
+
+  const onSignOut = async () => {
+    try {
+      const auth = getAuth(app);
+      await signOut(auth);
+      toast.success("로그아웃 되었습니다.");
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error?.code);
+    }
+  };
+
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__image" />
         <div>
-          <div className="profile__email">mephisto@naver.com</div>
-          <div className="profile__name">은동혁</div>
+          <div className="profile__email">{auth?.currentUser?.email}</div>
+          <div className="profile__name">
+            {auth?.currentUser?.displayName || "사용자"}
+          </div>
         </div>
       </div>
-      <Link to="/" className="profile__logout">로그아웃</Link>
+      <div role="presentation" className="profile__logout" onClick={onSignOut}>
+        로그아웃
+      </div>
     </div>
   );
 }

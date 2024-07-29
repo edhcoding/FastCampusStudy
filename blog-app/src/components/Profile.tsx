361e7fc452c3a5b3698f58b43191c6ab4f990873
eundonghyeok <mephisto7191@naver.com>
2024-07-29 18:00:27 +0900

    context api로 사용자 정보 관리하기

--- blog-app/src/components/Profile.tsx
+++ blog-app/src/components/Profile.tsx
@@ -1,30 +1,31 @@
+import AuthContext from "context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
+import { useContext } from "react";
 import { toast } from "react-toastify";
 
-export function Profile() {
-  const auth = getAuth(app);
-  console.log(auth);
+const onSignOut = async () => {
+  try {
+    const auth = getAuth(app);
+    await signOut(auth);
+    toast.success("로그아웃 되었습니다.");
+  } catch (error: any) {
+    console.log(error);
+    toast.error(error?.code);
+  }
+};
 
-  const onSignOut = async () => {
-    try {
-      const auth = getAuth(app);
-      await signOut(auth);
-      toast.success("로그아웃 되었습니다.");
-    } catch (error: any) {
-      console.log(error);
-      toast.error(error?.code);
-    }
-  };
+export function Profile() {
+  const { user } = useContext(AuthContext);
 
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__image" />
         <div>
-          <div className="profile__email">{auth?.currentUser?.email}</div>
+          <div className="profile__email">{user?.email}</div>
           <div className="profile__name">
-            {auth?.currentUser?.displayName || "사용자"}
+            {user?.displayName || "사용자"}
           </div>
         </div>
       </div>

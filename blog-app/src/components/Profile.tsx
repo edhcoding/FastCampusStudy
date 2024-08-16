bcdc029ec78998d1e02bd68ee7a03d6b7b6538e8
eundonghyeok <mephisto7191@naver.com>
2024-08-17 01:41:50 +0900

    Profile 수정

--- blog-app/src/components/Profile.tsx
+++ blog-app/src/components/Profile.tsx
@@ -29,6 +29,24 @@ export function Profile() {
           </div>
         </div>
       </div>
+      <div className="flex__box-lg">
+        <div className="profile__image" />
+        <div>
+          <div className="profile__email">{user?.email}</div>
+          <div className="profile__name">
+            {user?.displayName || "사용자"}
+          </div>
+        </div>
+      </div>
+      <div className="flex__box-lg">
+        <div className="profile__image" />
+        <div>
+          <div className="profile__email">{user?.email}</div>
+          <div className="profile__name">
+            {user?.displayName || "사용자"}
+          </div>
+        </div>
+      </div>
       <div role="presentation" className="profile__logout" onClick={onSignOut}>
         로그아웃
       </div>

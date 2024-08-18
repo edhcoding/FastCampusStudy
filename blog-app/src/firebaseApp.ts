653116c54605e397badc0fe8ee3efc61c47ebc7d
eundonghyeok <mephisto7191@naver.com>
2024-08-19 02:56:53 +0900

    Firestore란?

--- blog-app/src/firebaseApp.ts
+++ blog-app/src/firebaseApp.ts
@@ -4,11 +4,11 @@ import "firebase/auth"
 /**
  * - initializeApp - initializeApp 함수는 Firebase 애플리케이션을 초기화합니다. 이 함수는 Firebase 프로젝트의 구성 객체를 사용하여 Firebase 서비스를 설정합니다. 프로젝트에서 처음으로 Firebase를 사용할 때 호출해야 합니다.
  * - FirebaseApp - FirebaseApp은 Firebase SDK에서 제공하는 기본 객체로, Firebase 프로젝트를 초기화하고 다양한 Firebase 서비스를 사용할 수 있도록 해주는 역할을 합니다.
- * FirebaseApp 객체를 통해 Firebase 프로젝트와 상호작용할 수 있으며, 다양한 Firebase 서비스(예: Firestore, Authentication, Storage 등)를 사용할 때 이를 기반으로 설정을 관리합니다.
+ *   FirebaseApp 객체를 통해 Firebase 프로젝트와 상호작용할 수 있으며, 다양한 Firebase 서비스(예: Firestore, Authentication, Storage 등)를 사용할 때 이를 기반으로 설정을 관리합니다.
  * - getApp - getApp 함수는 초기화된 Firebase 애플리케이션 인스턴스를 반환합니다. 일반적으로 initializeApp 함수로 Firebase 애플리케이션을 초기화한 후, 다른 모듈이나 파일에서 동일한 Firebase 앱 인스턴스를 가져와야 할 때 사용합니다.
  */
 
-export let app: FirebaseApp;
+export let app: FirebaseApp; // app은 변수명 FirebaseApp은 타입
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -30,3 +30,17 @@ try {
 const firebase = initializeApp(firebaseConfig);
 
 export default firebase;
+
+// 원래 코드
+// import { initializeApp } from "firebase/app";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAvJUzM_Nfo9o16VpG6gvab4uJHeCs_5-o",
+//   authDomain: "toyprojects-c42c5.firebaseapp.com",
+//   projectId: "toyprojects-c42c5",
+//   storageBucket: "toyprojects-c42c5.appspot.com",
+//   messagingSenderId: "325369812734",
+//   appId: "1:325369812734:web:dee0bc71a845beb21337af"
+// };
+
+// const app = initializeApp(firebaseConfig);

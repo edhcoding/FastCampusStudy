b3cf59904145609c042aaa3218ccae2cf8d154f5
eundonghyeok <mephisto7191@naver.com>
2024-09-06 15:03:35 +0900

    page component 분리

--- twitter/src/App.tsx
+++ twitter/src/App.tsx
@@ -1,23 +1,8 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import Router from "components/Router";
 import "./App.css";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<h1>Home Page</h1>} />
-      <Route path="/posts" element={<h1>Home Page</h1>} />
-      <Route path="/posts:id" element={<h1>Home Page</h1>} />
-      <Route path="/posts/new" element={<h1>Home Page</h1>} />
-      <Route path="/posts/edit:id" element={<h1>Home Page</h1>} />
-      <Route path="/profile" element={<h1>Home Page</h1>} />
-      <Route path="/profile/edit" element={<h1>Home Page</h1>} />
-      <Route path="/notifications" element={<h1>Home Page</h1>} />
-      <Route path="/search" element={<h1>Home Page</h1>} />
-      <Route path="/users/login" element={<h1>Home Page</h1>} />
-      <Route path="/users/signup" element={<h1>Home Page</h1>} />
-      <Route path="*" element={<Navigate replace to="/" />} />
-    </Routes>
-  );
+  return <Router />;
 }
 
 export default App;

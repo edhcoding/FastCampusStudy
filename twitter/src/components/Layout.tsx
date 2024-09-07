db4d4ba5c0b86b367f5e0b9db03d806092b45a23
eundonghyeok <mephisto7191@naver.com>
2024-09-07 15:06:47 +0900

    MenuList 제작

--- twitter/src/components/Layout.tsx
+++ twitter/src/components/Layout.tsx
@@ -1,5 +1,11 @@
 import { ReactNode } from "react";
+import MenuList from "./Menu";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
-  return <div className="layout">{children}</div>;
+  return (
+    <div className="layout">
+      {children}
+      <MenuList />
+    </div>
+  );
 };

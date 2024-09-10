a173f4c9622fb4cf96ad1af692cb5b8b4fe0dffa
eundonghyeok <mephisto7191@naver.com>
2024-09-10 14:27:56 +0900

    firebase store로 데이터 생성하기 작업

--- twitter/src/components/posts/PostForm.tsx
+++ twitter/src/components/posts/PostForm.tsx
@@ -1,16 +1,69 @@
+import AuthContext from "context/AuthContext";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "firebaseApp";
+import { useContext, useState } from "react";
 import { FiImage } from "react-icons/fi";
+import { toast } from "react-toastify";
 
 export default function PostForm() {
+  const [content, setContent] = useState<string>("");
+
+  const { user } = useContext(AuthContext);
+
   const handleFileUpload = () => {};
 
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      await addDoc(collection(db, "posts"), {
+        // db , 컬렉션 이름, 데이터 내용
+        content,
+        createdAt: new Date().toLocaleDateString("ko", {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        }),
+        uid: user?.uid,
+        email: user?.email,
+      });
+      setContent("");
+      toast.success("게시글을 성공적으로 생성했습니다.");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // console.log(new Date()); // Tue Sep 10 2024 14:16:52 GMT+0900 (한국 표준시)
+  // console.log(new Date().toLocaleDateString()); // 2024. 9. 10.
+  // console.log(
+  //   new Date().toLocaleDateString("ko", {
+  //     hour: "2-digit",
+  //     minute: "2-digit",
+  //     second: "2-digit",
+  //   })
+  // ); // 2024. 9. 10. 오후 02:18:34
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "content") {
+      setContent(value);
+    }
+  };
+
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={onSubmit}>
       <textarea
         className="post-form__textarea"
         required
         name="content"
         id="content"
         placeholder="What is happening?"
+        value={content}
+        onChange={onChange}
       />
       <div className="post-form__submit-area">
         <label htmlFor="file-input" className="post-form__file">
--- twitter/src/pages/home/index.tsx
+++ twitter/src/pages/home/index.tsx
@@ -75,10 +75,12 @@ const posts: PostProps[] = [
 export default function HomePage() {
   return (
     <div className="home">
-      <div className="home__title">Home</div>
-      <div className="home__tabs">
-        <div className="home__tab home__tab--active">For You</div>
-        <div className="home__tab">Following</div>
+      <div className="home__top">
+        <div className="home__title">Home</div>
+        <div className="home__tabs">
+          <div className="home__tab home__tab--active">For You</div>
+          <div className="home__tab">Following</div>
+        </div>
       </div>
       <PostForm />
       <div className="post">

4e2a75388fc792197cc1c1edc7fc26d319e4d977
eundonghyeok <mephisto7191@naver.com>
2024-09-25 17:35:06 +0900

    Infinite Query, Intersection Observer로 무한 스크롤 구현

--- next-map/src/pages/api/stores.ts
+++ next-map/src/pages/api/stores.ts
@@ -1,32 +1,40 @@
-import { StoreApiResponse } from "@/interface";
+import { StoreApiResponse, StoreDataType } from "@/interface";
 import { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<StoreApiResponse>
+  res: NextApiResponse<StoreApiResponse | StoreDataType[]>
 ) {
-  const { page = "1" }: { page?: string } = req.query;
-  const skipPage = parseInt(page) - 1;
-
+  const { page = "" }: { page?: string } = req.query;
   const prisma = new PrismaClient();
-  const count = await prisma.store.count();
-  const stores = await prisma.store.findMany({
-    orderBy: { id: "asc" },
-    skip: skipPage * 10, // 2. 처음부터 10개 건너뛰고 그다음 11번째부터 보여줘! (offset)
-    take: 10, // 1. 한 번에 10개 데이터 보여줘! (limit)
-  });
 
-  // totalPage, page, data
+  if (page) {
+    const count = await prisma.store.count();
+    const skipPage = parseInt(page) - 1;
+    const stores = await prisma.store.findMany({
+      orderBy: { id: "asc" },
+      skip: skipPage * 10, // 2. 처음부터 10개 건너뛰고 그다음 11번째부터 보여줘! (offset)
+      take: 10, // 1. 한 번에 10개 데이터 보여줘! (limit)
+    });
 
-  // const stores = (await import("../../data/store_data.json"))[
-  //   "DATA"
-  // ] as StoreDataType[];
+    // totalPage, page, data
 
-  res.status(200).json({
-    page: parseInt(page),
-    data: stores,
-    totalCount: count,
-    totalPage: Math.ceil(count / 10),
-  });
+    // const stores = (await import("../../data/store_data.json"))[
+    //   "DATA"
+    // ] as StoreDataType[];
+
+    res.status(200).json({
+      page: parseInt(page),
+      data: stores,
+      totalCount: count,
+      totalPage: Math.ceil(count / 10),
+    });
+  } else {
+    const stores = await prisma.store.findMany({
+      orderBy: { id: "asc" },
+    });
+
+    return res.status(200).json(stores);
+  }
 }
--- next-map/src/pages/stores/index.tsx
+++ next-map/src/pages/stores/index.tsx
@@ -1,25 +1,73 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import Pagination from "@/components/\bPagination";
+import React, { useCallback, useEffect, useRef } from "react";
 import Loading from "@/components/Loading";
-import { StoreApiResponse } from "@/interface";
+import { StoreDataType } from "@/interface";
 import axios from "axios";
 import Image from "next/image";
-import { useRouter } from "next/router";
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
+import Loader from "@/components/Loader";
 
 export default function StoreListPage() {
-  const router = useRouter();
-  const { page = "1" }: any = router.query;
+  const ref = useRef<HTMLDivElement | null>(null);
+  const pageRef = useIntersectionObserver(ref, {});
+  const isPageEnd = !!pageRef?.isIntersecting; // 페이지 하단에 왔는지 안왔는지 확인하기 위해서
+
+  // const {
+  //   data: stores,
+  //   isLoading,
+  //   isError,
+  // } = useQuery(`stores-${page}`, async () => {
+  //   const { data } = await axios(`/api/stores?page=${page}`);
+  //   return data as StoreApiResponse;
+  // });
+
+  const fetchStores = async ({ pageParam = 1 }) => {
+    const { data } = await axios("/api/stores", {
+      params: {
+        limit: 10,
+        page: pageParam,
+      },
+    });
+
+    return data;
+  };
 
   const {
     data: stores,
+    isFetching,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
     isLoading,
     isError,
-  } = useQuery(`stores-${page}`, async () => {
-    const { data } = await axios(`/api/stores?page=${page}`);
-    return data as StoreApiResponse;
+  } = useInfiniteQuery("stores", fetchStores, {
+    getNextPageParam: (lastpage: any) =>
+      lastpage.data?.length > 0 ? lastpage.page + 1 : undefined,
+    // getNextPageParam. getPreviousPageParam: 다음 및 이전 페이지에 대한 매개 변수를 생성하는 함수
+    // getNextPageParam와 getPreviousPageParam 옵션으로 로드할 데이터가 더 있는지 여부와 fetch할 정보를 결정할 수 있습니다. 해당 정보는 query 함수에 추가 매개 변수로 제공됩니다.
   });
+
+  const fetchNext = useCallback(async () => {
+    const res = await fetchNextPage();
+
+    if (res.isError) {
+      console.log(res.error);
+    }
+  }, [fetchNextPage]);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout | undefined;
+
+    if (isPageEnd && hasNextPage) {
+      timerId = setTimeout(() => {
+        fetchNext();
+      }, 500);
+    }
+
+    return () => clearTimeout(timerId);
+  }, [fetchNext, hasNextPage, isPageEnd]);
 
   if (isError) {
     return (
@@ -35,44 +83,50 @@ export default function StoreListPage() {
         {isLoading ? (
           <Loading />
         ) : (
-          stores?.data?.map((store, i) => (
-            <li key={i} className="flex justify-between gap-x-6 py-5">
-              <div className="flex gap-x-4 ">
-                <Image
-                  src={
-                    store?.category
-                      ? `/images/markers/${store.category}.png`
-                      : "/images/markers/default.png"
-                  }
-                  alt="아이콘 이미지"
-                  width={48}
-                  height={48}
-                />
-                <div>
-                  <div className="text-sm font-semibold leading-6 text-gray-900">
-                    {store?.name}
+          stores?.pages?.map((page, i) => (
+            <React.Fragment key={i}>
+              {page.data.map((store: StoreDataType, i: number) => (
+                <li key={i} className="flex justify-between gap-x-6 py-5">
+                  <div className="flex gap-x-4 ">
+                    <Image
+                      src={
+                        store?.category
+                          ? `/images/markers/${store.category}.png`
+                          : "/images/markers/default.png"
+                      }
+                      alt="아이콘 이미지"
+                      width={48}
+                      height={48}
+                    />
+                    <div>
+                      <div className="text-sm font-semibold leading-6 text-gray-900">
+                        {store?.name}
+                      </div>
+                      <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
+                        {store?.storeType}
+                      </div>
+                    </div>
                   </div>
-                  <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
-                    {store?.storeType}
+                  <div className="hidden sm:flex sm:flex-col sm:items-end">
+                    <div className="text-sm font-semibold leading-6 text-gray-900">
+                      {store?.address}
+                    </div>
+                    <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
+                      {store?.phone || "번호없음"} | {store?.foodCertifyName} |{" "}
+                      {store?.category}
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="hidden sm:flex sm:flex-col sm:items-end">
-                <div className="text-sm font-semibold leading-6 text-gray-900">
-                  {store?.address}
-                </div>
-                <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
-                  {store?.phone || "번호없음"} | {store?.foodCertifyName} |{" "}
-                  {store?.category}
-                </div>
-              </div>
-            </li>
+                </li>
+              ))}
+            </React.Fragment>
           ))
         )}
       </ul>
-      {stores?.totalPage && (
+      {/* {stores?.totalPage && (
         <Pagination totalPage={stores?.totalPage} page={page} />
-      )}
+      )} */}
+      {(isFetching || hasNextPage || isFetchingNextPage) && <Loader />}
+      <div className="w-full touch-none h-10 mb-10" ref={ref} />
     </div>
   );
 }

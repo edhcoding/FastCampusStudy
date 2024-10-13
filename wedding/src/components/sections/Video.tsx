8ffc80757adf272200a955a9f94068b0a8e1e0f1
eundonghyeok <mephisto7191@naver.com>
2024-10-13 14:32:49 +0900

    인트로 동영상 구현

--- wedding/src/components/sections/Video.tsx
+++ wedding/src/components/sections/Video.tsx
@@ -1,4 +1,4 @@
-import styles from './Heading.module.scss'
+import styles from './Video.module.scss'
 import classNames from 'classnames/bind'
 import Section from '@shared/Section'
 
@@ -6,8 +6,16 @@ const cx = classNames.bind(styles)
 
 export default function Video() {
   return (
-    <Section>
-      <div className={cx('')}></div>
+    <Section className={cx('container')}>
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        poster="/assets/poster.jpg"
+      >
+        {/* source를 따로 뺀 상태에서 autoPlay를 주면 자동재생이 안되는데 muted를 주면 자동재생이 됨 */}
+        <source src="/assets/main.mp4" type="video/mp4" />
+      </video>
     </Section>
   )
 }
